d02ee74c243b1d5c4cdf0f4603abba821aa26079
Dylan Vuz <vuzdylan@gmail.com>
2016-02-18 10:12:46 -0500

    having page components does not actully make any sense when compnents become nested in routes

---
+++ app/js/components/Calendar.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+class Stock extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      month = 
+    }
+  }
+}
+
+export default Stock;
--- app/js/routes.jsx
+++ app/js/routes.jsx
@@ -2,15 +2,15 @@
 
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './page-components/app';
-import Home from './page-components/home';
-import Calendar from './page-components/calendar';
-import Portfolio from './page-components/portfolio';
+import App from './components/App';
+import Home from './components/Home';
+import CalendarPage from './containers/CalendarPage';
+import Portfolio from './components/Portfolio';
 
 export default (
   <Route component={App} path='/'>
     <IndexRoute component={Home}></IndexRoute>
-    <Route component={Calendar} path='/calendar'/>
+    <Route component={CalendarPage} path='/calendar'/>
     <Route component={Portfolio} path='/portfolio'/>
   </Route>
 )
