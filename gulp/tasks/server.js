9391197ac81178131db0748a4e684a48187584e3
Joshua Woodward <jdw6359@rit.edu>
2016-02-24 14:03:50 -0500

    Gulp to serve application on :8080

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -4,7 +4,7 @@ import gulp from 'gulp';
 import app from '../../server';
 
 gulp.task('server', ['build'], () => {
-  const server = app.listen(process.env.PORT || 3000, () => {
+  const server = app.listen(process.env.PORT || 8080, () => {
     console.log('Express server listening on port ' + server.address().port);
   });
 });
