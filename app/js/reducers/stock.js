86b248bf536915ca55b5963b0631d515b2aef53b
Matt Mokary <mxm6060@rit.edu>
2016-02-24 06:04:50 -0500

    airbnb js style guide is awesome

--- app/js/reducers/stock.js
+++ app/js/reducers/stock.js
@@ -3,8 +3,9 @@ import { ADD_STOCK } from '../actions/stock';
 export default function stocks(state = {}, action) {
   switch(action.type){
     case ADD_STOCK:
-      var update = {};
-      update[action.stock.symbol] = action.stock;
+      var update = {
+        [action.stock.symbol] : action.stock
+      };
       return Object.assign({}, state, update);
     default:
       return state;
