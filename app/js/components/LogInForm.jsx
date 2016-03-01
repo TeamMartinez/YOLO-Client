f712579cbbb3816a90606b1818029c6b8980acc7
Dylan Vuz <vuzdylan@gmail.com>
2016-03-01 16:58:33 -0500

    login form looks nice now

--- app/js/components/LogInForm.jsx
+++ app/js/components/LogInForm.jsx
@@ -5,10 +5,13 @@ import React from 'react';
 class LogInForm extends React.Component {
   render() {
     return (
-      <div>
-        <h2 className="ui header">Log in with git hub</h2>
-        <img src="http://www.molecularecologist.com/wp-content/uploads/2013/11/github-logo.jpg" />
-        <a className="ui primary button" href="/api/auth/github">Login</a>
+      <div style={{textAlign: "center"}}>
+        <div style={{textAlign: "center"}}>
+          <img src="http://www.molecularecologist.com/wp-content/uploads/2013/11/github-logo.jpg" />
+        </div>
+        <div style={{textAlign: "center"}}>
+          <a className="massive ui primary button" href="/api/auth/github">Login</a>
+        </div>
       </div>
     )
   }
