e24552540650cee44a36f8807519cf13bf42d83b
Dylan Vuz <vuzdylan@gmail.com>
2016-03-05 00:21:05 -0500

    just a little extra <3

--- app/js/containers/App.jsx
+++ app/js/containers/App.jsx
@@ -13,24 +13,46 @@ function mapStateToProps(state) {
 }
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.getLinkClasses = this.getLinkClasses.bind(this);
+  }
   componentDidMount() {
     this.props.dispatch(checkLogin());
   }
 
-  // TODO: Make the active class change depending on the page we're on
+  getLinkClasses() {
+    const current_page = this.props.location.pathname;
+    const classNames = {
+      home: "green item",
+      calendar: "blue item",
+      portfolio: "orange item"
+    }
+    if(current_page.indexOf("calendar") !== -1) {
+      classNames.calendar = "active blue item";
+    } else if(current_page.indexOf("portfolio") !== -1) {
+      classNames.portfolio = "active orange item";
+    } else {
+      classNames.home = "active green item";
+    }
+    return classNames;
+  }
+
   render(){
+    const classNames = this.getLinkClasses();
     return (
       <div className="ui container">
         <div className="ui three item labeled icon menu">
-          <Link className="green item" to="/">
+          <Link className={classNames.home} to="/">
             <i className="home icon"></i>
             Home
           </Link>
-          <Link className="blue item" to="/calendar">
+          <Link className={classNames.calendar} to="/calendar">
             <i className="calendar icon"></i>
             Calendar
           </Link>
-          <Link className="orange item" to="/portfolio">
+          <Link className={classNames.portfolio} to="/portfolio">
             <i className="line chart icon"></i>
             Portfolio
           </Link>
