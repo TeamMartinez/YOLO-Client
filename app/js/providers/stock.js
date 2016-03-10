d1d9402046fc21c225baa05f62b805f094e013c4
Matt Mokary <mxm6060@rit.edu>
2016-03-10 13:22:59 -0500

    abstract out url building for maybe including fetch

--- app/js/providers/stock.js
+++ app/js/providers/stock.js
@@ -2,15 +2,29 @@
 
 import moment from 'moment';
 
-export function fetchStocks(tickers, callback, failback) {
+function getStockURL(tickers) {
   const baseUrl = 'https://query.yahooapis.com/v1/public/yql?q=';
   const formatted = tickers.map(t => { return '"' + t + '"' }).join(',');
   const query = 
     'select * from yahoo.finance.quotes where symbol in (' + formatted + ')';
   const params =
     '&format=json&env=store://datatables.org/alltableswithkeys&callback=';
-  const url = baseUrl + encodeURI(query + params);
+  return baseUrl + encodeURI(query + params);
+}
 
+function getStockHistoryURL(ticker) {
+  const baseUrl = 'https://query.yahooapis.com/v1/public/yql?q=';
+  const query = 'select * from yahoo.finance.historicaldata where symbol="' +
+    ticker + '" and startDate="' + moment().subtract(1, 'years').format('YYYY-MM-DD') + '" and endDate="' + moment().format('YYYY-MM-DD') + '"';
+  const params =
+    '&format=json&env=store://datatables.org/alltableswithkeys&callback=';
+  return baseUrl + encodeURI(query + params);
+}
+
+export function fetchStocks(tickers, callback, failback) {
+  const url = getStockURL(tickers);
+
+  // use fetch?
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function () {
     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
@@ -25,13 +39,9 @@ export function fetchStock(ticker, callback, failback) {
 }
 
 export function fetchStockHistory(ticker, callback, failback) {
-  const baseUrl = 'https://query.yahooapis.com/v1/public/yql?q=';
-  const query = 'select * from yahoo.finance.historicaldata where symbol="' +
-    ticker + '" and startDate="' + moment().subtract(1, 'years').format('YYYY-MM-DD') + '" and endDate="' + moment().format('YYYY-MM-DD') + '"';
-  const params =
-    '&format=json&env=store://datatables.org/alltableswithkeys&callback=';
-  const url = baseUrl + encodeURI(query + params);
+  const url = getStockHistoryURL(ticker);
 
+  // use fetch?
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function () {
     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
