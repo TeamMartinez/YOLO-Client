10ce4884684ba40c865196cf8514990bc5a3f722
Matt Mokary <mxm6060@rit.edu>
2016-03-14 12:46:04 -0400

    add tests for priceFormatter's 'formatter' function

--- app/js/util/__tests__/priceFormatter_test.js
+++ app/js/util/__tests__/priceFormatter_test.js
@@ -2,7 +2,7 @@ jest.unmock('../priceFormatter');
 
 import { format, formatter } from '../priceFormatter';
 
-describe('priceFormatter', () => {
+describe('price_format', () => {
   
   it('should format the empty string to $0.00', () => {
     const price = '';
@@ -31,5 +31,33 @@ describe('priceFormatter', () => {
 
     expect(format(price, currency)).toEqual('$10.00');
   });
-  
+
+  it('should round 100.394857 to two decimal places', () => {
+    const price = 100.394857;
+    const currency = 'USD';
+
+    expect(format(price, currency)).toEqual('$100.39');
+  })
+
+});
+
+
+describe('price_formatter', () => {
+
+  it('should provide a formatter in USD', () => {
+    const price = '10';
+    const currency = 'USD';
+    const format = formatter(currency);
+
+    expect(format(price)).toEqual('$10.00');
+  });
+
+  it('should provide a formatter in GBP', () => {
+    const price = '10';
+    const currency = 'GBP';
+    const format = formatter(currency);
+
+    expect(format(price)).toEqual('£10.00');
+  })
+
 });
