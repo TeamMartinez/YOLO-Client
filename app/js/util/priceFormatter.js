d51ac1a4f3488ca42f9ca63ac204860b8a95f3d6
Matt Mokary <mxm6060@rit.edu>
2016-03-14 12:50:31 -0400

    add some comments in the priceFormatter and make variables less domain-specific

--- app/js/util/priceFormatter.js
+++ app/js/util/priceFormatter.js
@@ -7,11 +7,14 @@ const CURR_FORMATTERS = {
   'JPY' : '¥-'
 }
 
-export function format(price, currency) {
+// return the given amount as a string with the given currency's format
+export function format(amount, currency) {
   const unformatted = CURR_FORMATTERS[currency] || '-';
-  return unformatted.replace(/-/, parseFloat(price || 0).toFixed(2));
+  return unformatted.replace(/-/, parseFloat(amount || 0).toFixed(2));
 }
 
+// return a formatting function that uses the given currency's format and takes
+// only an amount
 export function formatter(currency) {
-  return price => { return format(price, currency) }
+  return amount => { return format(amount, currency) }
 }
