0397bad0cc8be21d69a4845e44be23e44a4cd191
Dylan Vuz <vuzdylan@gmail.com>
2016-05-09 11:16:07 -0400

    pushing to master like a g6

--- app/js/components/StockTickerItem.jsx
+++ app/js/components/StockTickerItem.jsx
@@ -8,17 +8,30 @@ class StockTickerItem extends React.Component {
   render() {
     const formattedPrice =
       format(this.props.stock.LastTradePriceOnly, this.props.stock.Currency);
-
-    return (
-      <div className="ui tiny inverted statistic">
-        <div className="green value">{formattedPrice}</div>
+    let change
+    if (this.props.stock.Change) {
+      change = (
         <div className="label">
-          <h2 className={this.props.stock.Change[0] === '+' ?
-              'ui green sub header' : 'ui red sub header'}>
+          <h2 className={this.props.stock.Change[0] === '+' ? 'ui green sub header' : 'ui red sub header'}>
             {this.props.stock.Symbol}
           </h2>
           <span>{this.props.stock.PercentChange}</span>
         </div>
+      )
+    } else {
+      change = (
+        <div className="label">
+          <h2 className="ui inverted sub header">
+            {this.props.stock.Symbol}
+          </h2>
+          <span>+0%</span>
+        </div>
+      )
+    }
+    return (
+      <div className="ui tiny inverted statistic">
+        <div className="green value">{formattedPrice}</div>
+        {change}
       </div>
     )
   }
